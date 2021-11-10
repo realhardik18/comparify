271df13b86471354a48bb6b1acc686639cead90b
hardik singh <realhardik18@gmail.com>
2021-11-10 09:35:08 +0530

    worked on getting data and then processing it and displaying output on result.html page

--- site_render.py
+++ site_render.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from threading import Thread
 from stats_getter import stats_getter
 
@@ -7,11 +7,22 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return render_template("index.html", stat1=stats_getter("x"), stat2=stats_getter("x"))
+    return render_template("index.html")
 
 
 def run():
     app.run(host='0.0.0.0')
+
+
+@app.route("/result", methods=['POST'])
+def result():
+    user_1_playlist = request.form['user_1']
+    user_1_playlist.split()
+    user_2_playlist = request.form['user_2']
+    user_2_playlist.split()
+    playlist_1 = user_1_playlist[34:]
+    playlist_2 = user_2_playlist[34:]
+    return render_template("result.html", user_1_stat=stats_getter(playlist_1), user_2_stat=stats_getter(playlist_2))
 
 
 def show_site():
