c4e5dd24dbd978566a379370ff512d72e137b7de
hardik singh <realhardik18@gmail.com>
2021-12-09 19:21:03 +0530

    error handling

--- site_render.py
+++ site_render.py
@@ -30,3 +30,13 @@ def result():
 def show_site():
     t = Thread(target=run)
     t.start()
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+    return render_template('500.html'), 500
